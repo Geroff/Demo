1718b5bccbbb1f1c65711b9e1c91bc2df7dec04f
Geroff <910683417@qq.com>
2017-08-03 16:35:52 +0800

    增加mac方法

--- common-lib/src/main/java/com/lgf/common/lib/utils/Utils.java
+++ common-lib/src/main/java/com/lgf/common/lib/utils/Utils.java
@@ -5,17 +5,28 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.net.NetworkInterface;
 import java.util.UUID;
 
 import static android.content.Context.TELEPHONY_SERVICE;
@@ -55,8 +66,12 @@ public class Utils {
 
     /**
      * "imei": "国际移动设备标识"
+     *
+     * @param context
+     * @param defaultValue
+     * @return
      */
-    public static String getImei(Context context) {
+    public static String getImei(Context context, String defaultValue) {
         if (context != null) {
             TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(TELEPHONY_SERVICE);
             String imei = telephonyManager.getDeviceId();
@@ -64,42 +79,51 @@ public class Utils {
                 return imei;
             }
         }
-        return "";
+        return defaultValue;
     }
 
     /**
      * "imsi": "国际移动用户识别码"
+     *
+     * @param context
+     * @param defaultValue
+     * @return
      */
-    public static String getImsi(Context context) {
+    public static String getImsi(Context context, String defaultValue) {
         if (context != null) {
             TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(TELEPHONY_SERVICE);
             return telephonyManager.getSubscriberId();
         }
 
-        return "";
+        return defaultValue;
     }
 
     /**
      * "phoneNumber": "手机号码"
+     *
+     * @param context
+     * @param defaultValue
+     * @return
      */
-    public static String getPhoneNumber(Context context) {
+    public static String getPhoneNumber(Context context, String defaultValue) {
         if (context != null) {
             TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(TELEPHONY_SERVICE);
             return telephonyManager.getLine1Number();
         }
 
-        return "";
+        return defaultValue;
     }
 
     /**
-     * 获取手机uuid,相当于加密androidId
+     * 获取手机uuid，相当于加密androidId
      */
     public static String getDeviceUUID(Context context) {
+        String defaultValue = "9774d56d682e549e";
         UUID uuid = null;
-        String androidId = getAndroidId(context);
+        String androidId = getAndroidId(context, defaultValue);
         // don't use wrong android id
-        if (androidId.equals("9774d56d682e549e")) {
-            String deviceId = getImei(context);
+        if (defaultValue.equals(androidId)) {
+            String deviceId = getImei(context, null);
             if (TextUtils.isEmpty(deviceId)) {
                 uuid = UUID.randomUUID();
             } else {
@@ -124,19 +148,107 @@ public class Utils {
      * 获取手机的androidId
      *
      * @param context
+     * @param defaultValue
      * @return
      */
-    public static String getAndroidId(Context context) {
-        String androidId = "9774d56d682e549e";
+    public static String getAndroidId(Context context, String defaultValue) {
         if (context != null) {
             try {
-                androidId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+                return Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
             } catch (Throwable e) {
                 e.printStackTrace();
             }
         }
 
-        return androidId;
+        return defaultValue;
+    }
+
+    /**
+     * "mac": "手机MAC地址"
+     */
+    public static String getMacAddress(Context context) throws Throwable {
+        if (Build.VERSION.SDK_INT >= 23) {
+            return getMacByReflect();
+        } else {
+            if (context != null) {
+                WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                return wifiInfo.getMacAddress();
+            } else {
+                return "00:00:00:00:00:00";
+            }
+        }
+    }
+
+    private static String getMacByReflect() throws Throwable {
+        String mac = null;
+
+        Class<?> clazz = Class.forName("android.os.SystemProperties"); // android.os.SystemProperties
+        Method get = clazz.getMethod("get", String.class);
+        String params = "wifi.interface";
+        String interfaceName = (String) get.invoke(clazz, params);
+        //        Log.i(MESSAGE_TAG, "interfaceName = " + interfaceName);
+
+        mac = getMacByName(interfaceName); // getMac from interfaceName
+        if (TextUtils.isEmpty(mac)) {
+            mac = getMacByFile(interfaceName);
+        }
+
+        if (!TextUtils.isEmpty(mac)) {
+            mac = mac.toLowerCase();
+        }
+
+        return mac;
+    }
+
+    private static String getMacByName(String interfaceName) throws Throwable {
+        NetworkInterface networkInterface = NetworkInterface.getByName(interfaceName);
+        byte[] addressBytes = networkInterface.getHardwareAddress();
+        if (addressBytes == null || addressBytes.length == 0) {
+            return null;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (byte addressByte : addressBytes) {
+            stringBuilder.append(String.format("%02X:", addressByte));
+        }
+
+        if (stringBuilder.length() > 0) {
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private static String getMacByFile(String interfaceName) {
+        StringBuilder buffer = new StringBuilder();
+        String fileName = "/sys/class/net/" + interfaceName + "/address";
+        InputStream is = null;
+        BufferedReader br = null;
+        try {
+            is = new FileInputStream(new File(fileName));
+            br = new BufferedReader(new InputStreamReader(is));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                buffer.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+
+                if (br != null) {
+                    br.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return buffer.toString();
     }
 
     /**
@@ -162,9 +274,10 @@ public class Utils {
      * 获取应用的版本
      *
      * @param context
+     * @param defaultValue
      * @return
      */
-    public static String getAppVersionName(Context context) {
+    public static String getAppVersionName(Context context, String defaultValue) {
         if (context != null) {
             PackageManager pm = context.getPackageManager();
             try {
@@ -174,7 +287,35 @@ public class Utils {
                 e.printStackTrace();
             }
         }
-        return "0.0.0";
+        return defaultValue;
+    }
+
+    /**
+     * 获取标签<meta-data>元数据的中name对应的value值
+     *
+     * @param context
+     * @param key
+     * @param defaultValue
+     * @return
+     */
+    private String getTokenFromMetaData(Context context, String key, String defaultValue) {
+        if (context == null) {
+            return defaultValue;
+        }
+
+        try {
+            ApplicationInfo applicationInfo = context.getPackageManager().getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+            if (applicationInfo != null) {
+                Bundle metaData = applicationInfo.metaData;
+                if (metaData != null) {
+                    return metaData.getString(key);
+                }
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return defaultValue;
     }
 
     /**
